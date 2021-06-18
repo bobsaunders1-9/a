87b690d54facb66eda7187afce8126bb961b7e66
bobsaunders1-9 <86120231+bobsaunders1-9@users.noreply.github.com>
2021-06-18 16:46:30 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="")
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+@client.command()
+async def hello(ctx):
+    await ctx.send("hi owner :D")
+
+client.run("ODQ4MTc5ODkxNTI5NzExNjI3.YLI3FA.THiDcCsWgdKKxyOoTOFksIlTEsM")
